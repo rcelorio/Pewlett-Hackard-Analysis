1fcafaa9b5ed21d8b99478fd8df5b5147709288e
Raul Celorio <rfcelorio@Rauls-MBP.lan>
2020-02-25 15:59:48 -0800

    Challenge submission

--- Queries/challenge.sql
+++ Queries/challenge.sql
@@ -26,4 +26,25 @@ WHERE
   rn = 1
   
  ORDER BY from_date DESC;
+
+  
+  -- Create new table for employees ready to mentor
+select tm.emp_no, tm.first_name, tm.last_name, tm.title, tm.from_date , tm.to_date
+into ready_for_mentor
+from (
+	SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date , t.to_date,
+		ROW_NUMBER() OVER 
+			(PARTITION BY (first_name, last_name) ORDER BY t.from_date DESC) rn
+	FROM employees e
+	inner join titles t
+		on e.emp_no = t.emp_no
+	inner join dept_emp de
+		on e.emp_no = de.emp_no
+	WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+	and de.to_date = '9999-01-01'
+	) tm
+where tm.rn = 1
+order by tm.last_name
+;
+
   
