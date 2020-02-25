38daf17991a9a6e67a8ce9412d60f580dbafcf81
Raul Celorio <rfcelorio@Rauls-MBP.lan>
2020-02-24 20:13:30 -0800

    created sql scripts

---
+++ Queries/schema.sql
@@ -0,0 +1,148 @@
+-- Table: public."Dept_Emp"
+
+-- DROP TABLE public."Dept_Emp";
+
+CREATE TABLE public."Dept_Emp"
+(
+    emp_no integer NOT NULL,
+    dept_no character varying(4) COLLATE pg_catalog."default" NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT "Dept_Emp_pkey" PRIMARY KEY (emp_no, dept_no),
+    CONSTRAINT "Dept_Emp_dept_no_fkey" FOREIGN KEY (dept_no)
+        REFERENCES public.departments (dept_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "Dept_Emp_emp_no_fkey" FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."Dept_Emp"
+    OWNER to postgres;
+
+-- Table: public."Managers"
+
+-- DROP TABLE public."Managers";
+
+CREATE TABLE public."Managers"
+(
+    dept_no character varying(4) COLLATE pg_catalog."default" NOT NULL,
+    emp_no integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT "Managers_pkey" PRIMARY KEY (emp_no, dept_no),
+    CONSTRAINT "Managers_dept_no_fkey" FOREIGN KEY (dept_no)
+        REFERENCES public.departments (dept_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "Managers_emp_no_fkey" FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."Managers"
+    OWNER to postgres;
+
+-- Table: public.departments
+
+-- DROP TABLE public.departments;
+
+CREATE TABLE public.departments
+(
+    dept_no character varying(4) COLLATE pg_catalog."default" NOT NULL,
+    dept_name character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT departments_pkey PRIMARY KEY (dept_no),
+    CONSTRAINT departments_dept_name_key UNIQUE (dept_name)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.departments
+    OWNER to postgres;
+
+-- Table: public.dept_manager
+
+-- DROP TABLE public.dept_manager;
+
+CREATE TABLE public.dept_manager
+(
+    dept_no character varying(4) COLLATE pg_catalog."default" NOT NULL,
+    emp_no integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT dept_manager_pkey PRIMARY KEY (emp_no, dept_no),
+    CONSTRAINT dept_manager_dept_no_fkey FOREIGN KEY (dept_no)
+        REFERENCES public.departments (dept_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT dept_manager_emp_no_fkey FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.dept_manager
+    OWNER to postgres;
+
+-- Table: public.employees
+
+-- DROP TABLE public.employees;
+
+CREATE TABLE public.employees
+(
+    emp_no integer NOT NULL,
+    birth_date date NOT NULL,
+    first_name character varying COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying COLLATE pg_catalog."default" NOT NULL,
+    gender character varying COLLATE pg_catalog."default" NOT NULL,
+    hire_date date NOT NULL,
+    CONSTRAINT employees_pkey PRIMARY KEY (emp_no)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.employees
+    OWNER to postgres;
+
+-- Table: public.salaries
+
+-- DROP TABLE public.salaries;
+
+CREATE TABLE public.salaries
+(
+    emp_no integer NOT NULL,
+    salary integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT salaries_pkey PRIMARY KEY (emp_no),
+    CONSTRAINT salaries_emp_no_fkey FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.salaries
+    OWNER to postgres;
